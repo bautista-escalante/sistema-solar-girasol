483336470cc862afff6656ce5af3a99254fd3fd8
bautista escalante <123372673+bautista-escalante@users.noreply.github.com>
2025-02-12 18:53:36 -0300

    Update sistema_solar_girasol1.ino

--- sistema_solar_girasol1.ino
+++ sistema_solar_girasol1.ino
@@ -1,42 +1,56 @@
 #include <SoftwareSerial.h>
 #include <Servo.h>
 
-Servo servoPanel; 
+Servo servoPanel;
 int grados = 0;
-int rendimiento = 0;
+int rendimientoOeste;
+int rendimientoEste;
+#define este A0
+#define oeste A1
 
 void setup() {
   Serial.begin(9600);
-  servoPanel.attach(9); 
+  servoPanel.attach(9);
 }
 
 void loop() {
-  
-  	rendimiento = obtenerRendimiento();
-	Serial.print(rendimiento);
-  	Serial.println("%");
-  
-	moverServo();
+
+  int valorEste = analogRead(este);
+  rendimientoEste = map(valorEste, 0, 512, 0, 100);
+  //Serial.print("valor este: ");
+  //Serial.println(valorEste);
+  Serial.print("rendimiento este; ");
+  Serial.print(rendimientoEste);
+  Serial.println("%");
+
+  int valorOeste = analogRead(oeste);
+  rendimientoOeste = map(valorOeste, 0, 512, 0, 100);
+  //Serial.print("valor oeste: ");
+  //Serial.println(valorOeste);
+  Serial.print("rendimiento oeste: ");
+  Serial.print(rendimientoOeste);
+  Serial.println("%");
+
+  moverServo();
+}
+
+void moverServo() {
+  int diferencia = abs(rendimientoOeste < rendimientoEste);
+  if (diferencia < 5) {
+    if (rendimientoOeste < rendimientoEste) {
+      grados += 10;
+    } else {
+      grados -= 10;
+    }
+    servoPanel.write(grados);
+    if (rendimientoOeste < 80 && rendimientoEste < 80 || grados > 180 || grados < 0) {
+      servoPanel.write(0);
+      grados = 0;
+    }
+    Serial.print("la posicion del panel es de ");
     Serial.print(grados);
     Serial.println(" Grados");
-  	
-}
-
-int obtenerRendimiento() {
-  int valor = analogRead(A5);
-  float voltaje = valor * (5.0 / 1023.0);
-  return map(voltaje, 0, 5, 0, 100);
-}
-
-void moverServo(){
-  if(rendimiento < 80){
-    
-    grados += 10;
-  	servoPanel.write(grados); 
-    delay(1000);
-  }
-  if(grados > 90){
-  	servoPanel.write(0);
-    grados = 0;
+    Serial.println("---------------------------");
+    delay(1500);
   }
 }
